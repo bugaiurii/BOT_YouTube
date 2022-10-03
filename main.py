44917ba80aa50d8e69e478730083f62478d0c9a2
Buga Iurii <bugaiurii@yandex.ru>
2022-10-03 13:39:12 +0300

    final step

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import *
 from config import *
 from pytube import YouTube
+import os
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
@@ -20,8 +21,16 @@ async def text_message(message:types.Message):
     if message.text.startswith == 'http://youtu.be' or 'http://www.youtube.com':
         await bot.send_message(chat_id, f"*Начинаю загрузку видео* : *{yt.title}*\n"
                                         f"* С канала *: [{yt.author}]({yt.channel_url})", parse_mode="Markdown")
+        await download_youtube_video(url, message, bot)
 
 
+async def download_youtube_video(url, message, bot):
+    yt = YouTube(url)
+    stream = yt.streams.filter(progressive=True, file_extension="mp4")
+    stream.get_highest_resolution().download(f'{message.chat.id}_{yt.title}')
+    with open(f"{message.chat.id}/{message.chat.id}_{yt.title}", 'rb') as video:
+        await bot.send_video(message.chat.id, video, caption="*Вот твой видос!*", parse_mode="Markdown")
+        os.remove(f"{message.chat.id}/{message.chat.id}_{yt.title}")
 
 
 
